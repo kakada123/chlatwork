3cd55fd93d455440795506a28dd0f9a511a6757d
Kakada <kadada.ng@aeoncambodia.com.kh>
2026-01-08 13:47:03 +0700

    update Analytics

--- app/app.vue
+++ app/app.vue
@@ -1,5 +1,10 @@
 <template>
+  <Analytics />
   <NuxtLayout>
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+import { Analytics } from "@vercel/analytics/nuxt";
+</script>
