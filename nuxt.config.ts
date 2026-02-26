581d50fcae78b7bd9b5100c5c0cf7f156ae2592f
Kakada <kadada.ng@aeoncambodia.com.kh>
2026-02-26 18:17:32 +0700

    change customer id on adsense

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -39,7 +39,7 @@ export default defineNuxtConfig({
       script: [
         {
           async: true,
-          src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3055328686081261",
+          src: "https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4280865455316436",
           crossorigin: "anonymous",
         },
       ],
